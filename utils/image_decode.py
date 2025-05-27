77558c0b5cc4b5fbdc682ac62bc59ecf9aff495c
kim kyunghoon <kkh9172@naver.com>
2025-05-27 14:21:55 +0900

    버전업

--- utils/image_decode.py
+++ utils/image_decode.py
@@ -1,8 +1,9 @@
-# utils/image_decode.py
 import base64
+import numpy as np
 from PIL import Image
 import io
 
 def decode_image(b64_string):
     image_data = base64.b64decode(b64_string)
-    return Image.open(io.BytesIO(image_data)).convert("L")
+    pil_img = Image.open(io.BytesIO(image_data)).convert("L")  # 흑백 변환
+    return np.array(pil_img)  # ✅ NumPy 배열로 변환
