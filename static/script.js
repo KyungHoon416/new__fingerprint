0b7ffe2b4fa7fbc033daa5d4da55a8d83c12b480
kim kyunghoon <kkh9172@naver.com>
2025-05-28 11:48:39 +0900

    커밋

--- static/script.js
+++ static/script.js
@@ -1,14 +1,9 @@
 const swiper = new Swiper(".mySwiper", {
-  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    dynamicBullets: true
   },
-  effect: 'fade',
-  speed: 700,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false
-  }
+  spaceBetween: 30,
+  loop: false,
+  effect: "slide",
 });
